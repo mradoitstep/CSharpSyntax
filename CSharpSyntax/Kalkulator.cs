391e39ff82577e21638cc8e99162a54128768dd0
CSharpFri <CSharpFri@CLASS-2-10>
2017-11-24 21:19:50 +0100

    feat

---
+++ CSharpSyntax/Kalkulator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSyntax
+{
+    class Kalkulator
+    {
+        public static int Scitaj(int a, int b)
+        {
+            int vysledok = a + b;
+            return vysledok;
+        }
+        public static int Scitaj(int a, int b, int c)
+        {
+            int vysledok = a + b + c;
+            return vysledok;
+        }
+        public static int Scitaj(string a, string b)
+        {
+            return int.Parse(a) + int.Parse(b);
+        }
+
+
+       
+
+
+    }
+
+    
+}
--- CSharpSyntax/Program.cs
+++ CSharpSyntax/Program.cs
@@ -8,8 +8,59 @@ namespace CSharpSyntax
 {
     class Program
     {
+        public int globalnaPremenna = 7;
+
+        void TestGlobalnejPremennej()
+        {
+
+        }
+
         static void Main(string[] args)
         {
+            const int mojaKonstanta = 4;
+            int polomer = 6;
+            //polomer = polomer + 1;
+            //polomer += 1;
+            Console.WriteLine("Povodny polomer {0}", polomer); // vypise 6
+            int novyPolomer = polomer++; // novzpolomer je stale 6 po ukonceni prikazu je ale 7 - vypse 6 !!! lebo ho zvzsi ay po operacii -2kroky=novyPomer=polomer a potom +1 ,
+            Console.WriteLine("Novy polomer {0}", novyPolomer);
+            int zaseNovyPolomer = ++polomer; // najprv zvysi 
+            Console.WriteLine("Zase novy polomer{0}", zaseNovyPolomer);
+
+            //pouyitie kalkulatoru
+            Kalkulator.Scitaj("4", "5");
+
+
+            int vysledok = 5;
+
+            switch (vysledok)
+            {
+                case 2:
+                    Console.WriteLine("Vysledok bol 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Vysledok bol 3");
+                    break;
+                case 4:
+                case 5:
+                    Console.WriteLine("Vysledok bol 4 aj 5");
+                    break;
+                
+
+            }
+
+
+            // 10-20 nebo 40-60 vcetne, a zaroven nesmie byt 16 !
+            if (((vysledok >= 10 && vysledok <= 20) || (vysledok >= 40 && vysledok <= 60)) && vysledok !=16);
+
+
+
+
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
